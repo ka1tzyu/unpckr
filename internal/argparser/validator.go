a4ea880a45b9a2a324d47bdf4b97645c104dd3d8
nekovalue <nekovalue@gmail.com>
2021-07-10 10:42:44 +0300

    [UV-A] Validators

--- internal/argparser/validator.go
+++ internal/argparser/validator.go
@@ -1,6 +1,23 @@
 package argparser
 
-// TODO: VALIDATORS
-func sourceFoldersValidate(args []string) error     { return nil }
-func destinationFolderValidate(args []string) error { return nil }
-func patternValidate(args []string) error           { return nil }
+import (
+	"fmt"
+	"os"
+)
+
+func sourceFoldersValidate(args []string) error {
+	if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+		return fmt.Errorf("validate: source %v is not exist", args[0])
+	}
+	return nil
+}
+func destinationFolderValidate(args []string) error {
+	if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+		err := os.MkdirAll(args[0], 0750)
+		return err
+	}
+	return nil
+}
+func patternValidate(args []string) error {
+	return nil
+}
